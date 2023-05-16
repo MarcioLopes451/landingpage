4e3e503347cfed22251b92951c67093cbb358655
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-05-16 23:32:07 +0100

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../../images/logo.svg';
+
+export default function Navbar() {
+  return (
+    <header className='headerOne'>
+        <div className='navbar-container'>
+            <a href='#'> <img src={Logo} alt='snap'/></a>
+            <div>
+                
+            </div>
+        </div>
+    </header>
+  )
+}
