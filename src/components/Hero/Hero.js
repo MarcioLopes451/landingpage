e354b94be3a7249a2bb741d76ba83d4e687cdae8
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-05-18 15:46:47 +0100

    desktop finished

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import HeroImage from '../../images/image-hero-desktop.png';
+import Databiz from '../../images/client-databiz.svg';
+import Audiophlie from '../../images/client-audiophile.svg';
+import Meet from '../../images/client-meet.svg';
+import Maker from '../../images/client-maker.svg';
 
 export default function Hero() {
   return (
     <section className='hero-section'>
         <div className='hero-container'>
             <div className='intro'>
-                <h1>Make Remote Work</h1>
+                <h1>Make <br />
+                Remote Work</h1>
                 <p> Get your team in sync, no matter your location. 
                     Streamline processes, create team rituals, 
                     and watch productivity soar
                 </p>
                 <button>Learn more</button>
+                <div className='clients'>
+                    <img src={Databiz} alt='databiz'/>
+                    <img src={Audiophlie} alt='audiophlie'/>
+                    <img src={Meet} alt='meet'/>
+                    <img src={Maker} alt='maker'/>
+                </div>
             </div>
             <div className='hero-image'>
                 <img src={HeroImage} alt='hero' />
