8315f37e55250c03bfd636d949753ae55ea070e3
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-05-19 12:59:20 +0100

    dropdown menu finished

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from '../../images/logo.svg';
 import Menu from '../../images/icon-menu.svg';
+import Todo from '../../images/icon-todo.svg';
+import Calender from '../../images/icon-calendar.svg';
+import Reminder from '../../images/icon-reminders.svg';
+import Planning from '../../images/icon-planning.svg';
+import ArrowUp from '../../images/icon-arrow-up.svg';
+import ArrowDown from '../../images/icon-arrow-down.svg';
+
 
 export default function Navbar() {
+const [open,setOpen] = useState(false);
+const [open1,setOpen1] = useState(false);
+
+function handleOpen(){
+  setOpen(!open,ArrowUp);
+}
+function handleOpen1(){
+  setOpen1(!open1);
+}
+
+
   return (
     <header className='headerOne'>
         <div className='navbar-container'>
             <div className='nav'>
             <a href='#'> <img src={Logo} alt='snap'/></a>
-            <a href='#' className='links'>Features</a>
-            <a href='#' className='links'>Company</a>
+            <button className='nested-links' onClick={handleOpen}>Features 
+            {open ? (
+              <img src={ArrowUp} alt='up' />
+            ): <img src={ArrowDown} alt='down'/>}
+            </button>
+            <button className='nested-links' onClick={handleOpen1}>Company
+            {open1 ? (
+              <img src={ArrowUp} alt='up' />
+            ): <img src={ArrowDown} alt='down'/>}
+            </button>
             <a href='#' className='links'>Careers</a>
             <a href='#' className='links'>About</a>
             </div>
@@ -19,6 +45,21 @@ export default function Navbar() {
             </div>
           <img src={Menu} alt='menu' className='menu-icon' />
         </div>
+        {open ? (
+              <ul className='nested-lists'>
+                <li> <img src={Todo} alt='todo'/>Todo List</li>
+                <li> <img src={Calender} alt='calender'/>Calender</li>
+                <li> <img src={Reminder} alt='reminder'/>Reminders</li>
+                <li> <img src={Planning} alt='Planning'/>Planning</li>
+              </ul>
+            ) : null}
+            {open1 ? (
+              <ul className='nested-lists-two'>
+                <li>History</li>
+                <li>Our Team</li>
+                <li>Blog</li>
+              </ul>
+            ) : null}
     </header>
   )
 }
